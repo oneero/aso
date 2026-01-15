14af8ecbe42eb16a77b413b7dc612cd4710f63a6
oneero <eero@oneeronaut.com>
2026-01-15 19:29:42 +0200

    switch from nix to distrobox for env

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # aso
 Vulkan sandbox
+
+# env
+
+You can find a list of dependencies in the install_deps.sh script. Distrobox is likely the easiest way to setup a containerized environment with the necessary dependencies without contaminating your system configuration.
+
+1. Ensure your system can run vulkaninfo / vkcube first.
+2. Setup distrobox environment:
+```bash
+distrobox create --name aso-dev --image archlinux:latest --nvidia # leave out if AMD
+distrobox enter aso-dev
+./install_deps.sh
+```
---
+++ install_deps.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+packages=(
+  base-devel
+
+  # Vulkan
+  vulkan-headers
+  vulkan-tools
+  vulkan-validation-layers
+
+  # Shaders
+  shaderc
+  spirv-tools
+  glslang
+
+  # SDL3
+  sdl3
+
+  # Wayland
+  wayland
+  wayland-protocols
+  libxkbcommon
+
+  # Build tools
+  cmake
+  gcc
+  ninja
+  pkg-config
+
+  # Development
+  gdb
+  # renderdoc
+  # tracy
+)
+
+echo "Installing dependencies..."
+sudo pacman -Sp --needed "${packages[@]}"
+sudo pacman -S --needed "${packages[@]}"
+echo "Done."
