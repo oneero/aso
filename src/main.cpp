bf26dce2fb3a955572e03bd1f8335b8f63b1365a
oneero <eero@oneeronaut.com>
2026-02-10 21:47:10 +0200

    suppress lsan leaks from drivers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "aso.h"
 
+#if defined(__SANITIZE_ADDRESS__)
+extern "C" const char *__lsan_default_suppressions() {
+  return "leak:<unknown module>\n";
+}
+#endif
+
 int main(int argc, char *argv[]) {
   aso_ctx ctx = {0};
   aso_init(&ctx);
