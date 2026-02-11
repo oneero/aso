9f6d31113227b8f0a717fd517ae0094672ccd631
oneero <eero@oneeronaut.com>
2026-02-11 16:26:33 +0200

    more macros + replacing logging

--- src/core.h
+++ src/core.h
@@ -1,11 +1,91 @@
 #ifndef ASO_CORE_H
 #define ASO_CORE_H
 
-#include <cstdarg>
 #include <cstdint>
-#include <cstdio>
 
-// types
+// REGION: COMPILER
+
+#if defined(__clang__)
+  #define COMPILER_CLANG 1
+#elif defined(_MSC_VER)
+  #define COMPILER_MSVC 1
+#elif defined(__GNUC__)
+  #define COMPILER_GCC 1
+#else
+  #error "Unsupported compiler"
+#endif
+
+#if !defined(COMPILER_CLANG)
+  #define COMPILER_CLANG 0
+#endif
+#if !defined(COMPILER_MSVC)
+  #define COMPILER_MSVC 0
+#endif
+#if !defined(COMPILER_GCC)
+  #define COMPILER_GCC 0
+#endif
+
+// REGION: OS
+
+#if defined(_WIN32)
+  #define OS_WINDOWS 1
+#elif defined(__linux__)
+  #define OS_LINUX 1
+#else
+  #error "Unsupported OS"
+#endif
+
+#if !defined(OS_WINDOWS)
+  #define OS_WINDOWS 0
+#endif
+#if !defined(OS_LINUX)
+  #define OS_LINUX 0
+#endif
+
+// REGION: ARCH
+
+#if defined(__x86_64__) || defined(_M_AMD64)
+  #define ARCH_X64 1
+#elif defined(__i386__) || defined(_M_IX86)
+  #define ARCH_X86 1
+#elif defined(__aarch64__) || defined(_M_ARM64)
+  #define ARCH_ARM64 1
+#elif defined(__arm__) || defined(_M_ARM)
+  #define ARCH_ARM32 1
+#else
+  #error "Unsupported architecture"
+#endif
+
+#if !defined(ARCH_X64)
+  #define ARCH_X64 0
+#endif
+#if !defined(ARCH_X86)
+  #define ARCH_X86 0
+#endif
+#if !defined(ARCH_ARM64)
+  #define ARCH_ARM64 0
+#endif
+#if !defined(ARCH_ARM32)
+  #define ARCH_ARM32 0
+#endif
+
+// REGION: BUILD TYPE
+
+#if !defined(BUILD_DEBUG)
+  #define BUILD_DEBUG 0
+#endif
+#if !defined(BUILD_RELEASE)
+  #define BUILD_RELEASE 0
+#endif
+
+// default to debug
+#if !BUILD_DEBUG && !BUILD_RELEASE
+  #undef  BUILD_DEBUG
+  #define BUILD_DEBUG 1
+#endif
+
+// REGION: TYPES
+
 typedef int8_t i8;
 typedef int32_t i32;
 typedef int64_t i64;
@@ -14,7 +94,54 @@ typedef uint32_t u32;
 typedef uint64_t u64;
 typedef float f32;
 
-// util
+// REGION: MACROS
+
+#define STMNT(s) do { s } while(0)
+
+// REGION: ASSERT
+
+// trap
+#if COMPILER_MSVC
+  #define DEBUG_TRAP __debugbreak()
+#elif COMPILER_CLANG
+  #define DEBUG_TRAP __builtin_debugtrap()
+#elif COMPILER_GCC
+  // GCC doesn't have __builtin_debugtrap, so:
+  #if ARCH_X64
+    #define DEBUG_TRAP __asm__ __volatile__("int3")
+  #elif ARCH_ARM64
+    #define DEBUG_TRAP __asm__ __volatile__("brk #0xf000")
+  #else
+    #define DEBUG_TRAP __builtin_trap()  // Last resort on GCC
+  #endif
+#else
+  #define DEBUG_TRAP (*(volatile int*)0 = 0)
+#endif
+
+// never stripped
+#define ASSERT(expr)                                   \
+  STMNT(                                               \
+      if (!(expr)) {                                   \
+        fprintf(stderr, "ASSERT FAILED: %s\n %s:%d\n", \
+                #expr, __FILE__, __LINE__);            \
+        DEBUG_TRAP;                                    \
+      })
+
+// stripped
+#if BUILD_DEBUG
+  #define D_ASSERT(expr) ASSERT(expr)
+#else
+  #define D_ASSERT(expr) ((void)0)
+#endif
+
+// REGION: LOGGING
+
+#define LOG(fmt, ...)        fprintf(stdout, fmt "\n", ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...)  fprintf(stderr, "[ERROR] %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LogWarning(fmt, ...) fprintf(stderr, "[WARN]  %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+// REGION: UTILITY / MATH
+
 #define KB(n) ((n) * 1024ull)
 #define MB(n) (KB(n) * 1024ull)
 #define GB(n) (MB(n) * 1024ull)
@@ -25,16 +152,5 @@ typedef float f32;
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 #define SIGN(x) ((x) > 0 ? 1 : ((x) < 0 ? -1 : 0))
 #define SQUARE(x) ((x) * (x))
-
-// TODO: add proper logging and assertion
-
-// logging function
-// currently simply wraps printf
-static inline void aso_log(const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  vprintf(fmt, args);
-  va_end(args);
-}
 
 #endif // ASO_CORE_H
