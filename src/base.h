b7e9bbe5132a3cddb82a74f5cf8fb220ce09b58c
oneero <eero@oneeronaut.com>
2026-02-21 15:34:12 +0200

    added missing typedef

--- src/base.h
+++ src/base.h
@@ -88,9 +88,11 @@
 // REGION: TYPES
 
 typedef int8_t   i8;
+typedef int16_t  i16;
 typedef int32_t  i32;
 typedef int64_t  i64;
 typedef uint8_t  u8;
+typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 typedef float    f32;
